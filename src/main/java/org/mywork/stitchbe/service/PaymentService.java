0c0f0bdf577262c5a93fea34b7d408f0e555608c
dar._.ho <65858323+kim-hoyoung@users.noreply.github.com>
2024-09-25 14:59:06 +0900

    결제 정보관리 완

--- src/main/java/org/mywork/stitchbe/service/PaymentService.java
+++ src/main/java/org/mywork/stitchbe/service/PaymentService.java
@@ -4,6 +4,7 @@
 설명 : 결제 service.
 _____________________
 2024.9.24 박요한 | 생성.
+2024.9.25 김호영 | 결제 정보 확인 구현.
 */
 
 package org.mywork.stitchbe.service;
@@ -12,6 +13,8 @@ import lombok.RequiredArgsConstructor;
 import org.mywork.stitchbe.dto.PaymentDTO;
 import org.mywork.stitchbe.mapper.PaymentMapper;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor // Lombok을 사용하여 의존성 자동 주입
@@ -27,4 +30,14 @@ public class PaymentService {
         // 결제 정보 DB에 저장
         paymentMapper.insertPayment(paymentDTO);
     }
+    // 모든 결제 정보 조회 (관리자용)
+    public List<PaymentDTO> getAllPayments() {
+        return paymentMapper.getAllPayments();
+    }
+    // 결제 상태 업데이트
+    public void updatePaymentStatus(Long paymentId, String status) {
+        paymentMapper.updatePaymentStatus(paymentId, status);
+    }
+
+
 }
