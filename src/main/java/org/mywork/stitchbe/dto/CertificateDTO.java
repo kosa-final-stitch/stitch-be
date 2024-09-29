8480234e413e7357fd3c85ae62de9c7e58c3a4f6
dar._.ho <65858323+kim-hoyoung@users.noreply.github.com>
2024-09-30 00:34:02 +0900

    FIX : 수료 dto 에nickname 추가

--- src/main/java/org/mywork/stitchbe/dto/CertificateDTO.java
+++ src/main/java/org/mywork/stitchbe/dto/CertificateDTO.java
@@ -17,8 +17,9 @@ public class CertificateDTO {
     private Long certificateId;    // 수료증 ID
     private Long memberId;         // 회원 ID
     private Long courseId;         // 과정 ID
-    private String courseName;     // 수료 과정명 (조인으로 가져온다)
-    private String academyName;    // 학원명 (조인으로 가져온다)
+    private String courseName;     // 수료 과정명 (조인)
+    private String academyName;    // 학원명 (조인)
+    private String nickname;       // 회원 닉네임 (조인으로 가져옴)
     private Date completionDate;   // 수료 날짜
     private String status;         // 상태 ('미처리', '인증', '불가')
     private String filename;       // 수료증 파일명
