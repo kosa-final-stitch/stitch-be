4787bcd6e6128a704f5eced6baf1e257b5ae4bf8
dar._.ho <65858323+kim-hoyoung@users.noreply.github.com>
2024-09-29 23:50:13 +0900

    ADD: 수료 과목 인증 조회 등록 기능 구현

---
+++ src/main/java/org/mywork/stitchbe/mapper/CertificateMapper.java
@@ -0,0 +1,27 @@
+/*
+담당자: 김호영
+시작 일자: 2024.09.29
+설명 : 수료과목 인증 Mapper
+---------------------
+2024.09.29 김호영 | 수료 과목 인증 insert, select Mapper 생성.
+*/
+
+package org.mywork.stitchbe.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.mywork.stitchbe.dto.CertificateDTO;
+
+import java.util.List;
+
+@Mapper
+public interface CertificateMapper {
+
+    // 수료증 등록
+    void insertCertificate(CertificateDTO certificate);
+
+    // 로그인된 사용자의 수료증 항목 조회
+    List<CertificateDTO> selectCertificatesByMemberId(Long memberId);
+
+    // 모든 수료 항목 조회 (관리자용)
+    List<CertificateDTO> selectAllCertificates();
+}
