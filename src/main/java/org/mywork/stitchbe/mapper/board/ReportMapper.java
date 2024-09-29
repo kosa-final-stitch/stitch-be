7d902f554bb11b0ee3a2e255b536d99a2e7b20c1
dar._.ho <65858323+kim-hoyoung@users.noreply.github.com>
2024-09-29 19:00:47 +0900

    ADD: 신고 데이터 조회

--- src/main/java/org/mywork/stitchbe/mapper/board/ReportMapper.java
+++ src/main/java/org/mywork/stitchbe/mapper/board/ReportMapper.java
@@ -1,12 +1,26 @@
+/*
+ 담당자:
+ 시작 일자: 2024.09.24
+ 설명 : 신고 구현 개발
+ ---------------------
+ 2024.09.29 김호영 | 신고목록 조회 기능 추가.
+ */
+
 package org.mywork.stitchbe.mapper.board;
 
 import org.apache.ibatis.annotations.Mapper;
 import org.mywork.stitchbe.dto.board.ReportDto;
 
+import java.util.List;
+
 @Mapper
 public interface ReportMapper {
     // 신고 데이터 삽입
     void insertReport(ReportDto reportDto);
+
+    // 신고 목록 조회 (호영)
+    List<ReportDto> selectReports(); // 추가된 신고 목록 조회 메서드
+
 //
 //    // 특정 신고 데이터 조회 (필요 시)
 //    ReportDto selectReportById(Long reportId);
